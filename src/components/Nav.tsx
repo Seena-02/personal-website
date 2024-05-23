5da7b05e99a1f9d620f71dfe7de79e4e84b711f7
Seena Mohajeran <seenamohajeran@gmail.com>
2024-05-22 21:34:36 -0700

    add nav bar boiler plate

---
+++ src/components/Nav.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <h1>
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src=""
+              alt="Logo"
+              width="30"
+              height="24"
+              className="d-inline-block align-text-top"
+            />
+            Seena Mohajeran
+          </a>
+        </div>
+      </nav>
+    </h1>
+  );
+}
+
+export default Nav;
