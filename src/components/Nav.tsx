3f970487645e25c4286a517e2f6c98c5693da647
Seena Mohajeran <seenamohajeran@gmail.com>
2024-05-28 22:50:04 -0700

    add relevant navbar items

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,57 +1,60 @@
 interface NavProps {
-  brandName : string,
-  imageSrcPath : string;
+  brandName: string;
+  imageSrcPath: string;
 }
 
-function Nav({brandName, imageSrcPath} : NavProps) {
+function Nav({ brandName, imageSrcPath }: NavProps) {
   return (
-      <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img
-              src={imageSrcPath}
-              alt="Logo"
-              width="60"
-              height="60"
-              className="d-inline-block align-center"
-            />
-            <span className="fw-bolder fs-4"> 
-            {brandName}
-            </span>
-          </a>
+    <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={imageSrcPath}
+            alt="Logo"
+            width="60"
+            height="60"
+            className="d-inline-block align-center"
+          />
+          <span className="fw-bolder fs-4">{brandName}</span>
+        </a>
 
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Publications
+              </a>
+            </li>
           </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
 
